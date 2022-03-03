351d0ee4f18b99f125ca2d5ebfa0a6c222c79974
Kai Majerus <kaimajerus93@gmail.com>
2022-03-03 17:40:58 +0000

    adding GCP configuration to Makefile

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,11 +1,13 @@
 import mlflow
+import joblib
 
-from  mlflow.tracking import MlflowClient
+from mlflow.tracking import MlflowClient
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from memoized_property import memoized_property
 
@@ -19,7 +21,7 @@ class Trainer():
     MLFLOW_URI = "https://mlflow.lewagon.co/"
     EXPERIMENT_NAME = "[GB] [London] [kai-majerus] TaxiFareModel 1.0"
 
-    def __init__(self, X, y):
+    def __init__(self, X, y, model):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -27,6 +29,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.model = model
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -42,20 +45,21 @@ class Trainer():
             ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
             ('time', time_pipe, ['pickup_datetime'])
         ], remainder="drop")
-        pipe = Pipeline([
+        self.pipeline = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            ('model', self.model)
         ])
-        return pipe
+        return self
 
     def run(self):
         """set and train the pipeline"""
-        self.pipeline = Trainer(self.X, self.y).set_pipeline()
         self.pipeline.fit(self.X, self.y)
+        return self
 
-    def evaluate(self, X_test, y_test):
+    def evaluate(self, X_test, y_test, cross_val=False):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
+        
         rmse = compute_rmse(y_pred, y_test)
         return rmse
     
@@ -80,26 +84,38 @@ class Trainer():
 
     def mlflow_log_metric(self, key, value):
         self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+    
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, 'model.joblib')
 
 
 if __name__ == "__main__":
     # store the data in a DataFrame
     df = get_data()
+
     # clean the data
     df = clean_data(df)
+
     # set X and y
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
+
     # hold out
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
-    # train
-    trainer = Trainer(X_train, y_train)
-    trainer.run()
-    # evaluate
-    rmse = trainer.evaluate(X_val, y_val)
-    # create experiment and log model name and metric
-    trainer.mlflow_log_metric("rmse", rmse)
-    trainer.mlflow_log_param("model", "LinearRegression")
+
+    # train and fit and evaluate
+    for model in [LinearRegression(), RandomForestRegressor()]:
+        trainer = Trainer(X_train, y_train, model)
+        rmse = trainer.set_pipeline().run().evaluate(X_val, y_val)
+        # create experiment and log model name and metric
+        trainer.mlflow_log_metric("rmse", rmse)
+        trainer.mlflow_log_param("model", model)
+
     # retrieve experiment ID
     experiment_id = trainer.mlflow_experiment_id
     print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
+
+    # save model best model
+    # best_model = 
+    # trainer.save_model()
